d6a12b89e57a3b2f077e651166f4e9186435fe4e
ben <goldenbergben@gmail.com>
2017-03-11 22:19:19 +0200

    added jade

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  // app.set('views', __dirname + '/views');
-  // app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
